8e175d957f7ffc7405e4089033185833346defc0
Wrench56 <dmarkreg@gmail.com>
2023-07-08 21:08:26 +0200

    Use one dark color theme

--- config.h
+++ config.h
@@ -113,28 +113,26 @@ float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
+	"#282c34", /* 0: black */
+	"#e06c75", /* 1: red */
+	"#98c379", /* 2: green */
+	"#d19a66", /* 3: yellow */
+	"#61afef", /* 4: blue */
+	"#c678dd", /* 5: magenta */
+	"#56b6c2", /* 6: cyan */
+	"#abb2bf", /* 7: white */
+	"#5c6370", /* 8: brblack */
+	"#e06c75", /* 9: brred */
+	"#98c379", /* 10: brgreen */
+	"#d19a66", /* 11: bryellow */
+	"#61afef", /* 12: brblue */
+	"#c678dd", /* 13: brmagenta */
+	"#56b6c2", /* 14: brcyan */
+	"#ffffff", /* 15: brwhite */
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
-	"#ebdbb2", /* 259 -> fg */
 };
 
 
@@ -142,11 +140,11 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 259;
-unsigned int defaultbg = 258;
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 0;
 unsigned int defaultcs = 256;
 unsigned int defaultrcs = 257;
-unsigned int background = 258;
+unsigned int background = 0;
 
 /*
  * Default shape of cursor
@@ -206,8 +204,8 @@ ResourcePref resources[] = {
 		{ "color13",      STRING,  &colorname[13] },
 		{ "color14",      STRING,  &colorname[14] },
 		{ "color15",      STRING,  &colorname[15] },
-		{ "background",   STRING,  &colorname[258] },
-		{ "foreground",   STRING,  &colorname[259] },
+		{ "background",   STRING,  &colorname[0] },
+		{ "foreground",   STRING,  &colorname[7] },
 		{ "cursorColor",  STRING,  &colorname[256] },
 		{ "termname",     STRING,  &termname },
 		{ "shell",        STRING,  &shell },
